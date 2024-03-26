399c9846f98bbebb9caaeac79eb402a2aabcddfe
Polat Avcı <polatavci2222@gmail.com>
2024-03-26 20:05:53 +0300

    restaurant page'e comment eklendi

--- lib/screens/restaurant_page.dart
+++ lib/screens/restaurant_page.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:yemeksepeti/components/comment.dart";
 import "package:yemeksepeti/components/restaurant_menu.dart";
 import "package:yemeksepeti/main.dart";
 import "package:yemeksepeti/screens/home_screen.dart";
@@ -30,26 +31,83 @@ class RestaurantPage extends StatelessWidget {
               ))
         ],
       ),
-      body: Column(
+      body: ListView(
         children: [
-          Row(
-            children: [
-              RestaurantMenu(
-                title: "Big Mac Menü",
-                image: Images.burger,
-                onTap: () {},
-                price: "230 TL",
-                widgetCount: 2,
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(children: [
+              Row(
+                children: [
+                  Icon(Icons.favorite, color: AppTheme.primaryColor),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  const Text(
+                    "Popüler",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+                  ),
+                ],
               ),
-              RestaurantMenu(
-                title: "Big Mac Menü",
-                image: Images.burger,
-                onTap: () {},
-                price: "230 TL",
-                widgetCount: 2,
+              const Padding(
+                padding: EdgeInsets.only(left: 5),
+                child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Restoranın en çok tercih edilen ürünleri")),
               ),
-            ],
-          )
+              Row(
+                children: [
+                  RestaurantMenu(
+                    title: "Big Mac Menü",
+                    image: Images.burger,
+                    onTap: () {},
+                    price: "230 TL",
+                    widgetCount: 2,
+                  ),
+                  RestaurantMenu(
+                    title: "Big Mac Menü",
+                    image: Images.burger,
+                    onTap: () {},
+                    price: "230 TL",
+                    widgetCount: 2,
+                  ),
+                ],
+              ),
+            ]),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(color: Colors.grey[200]),
+            child: Column(children: [
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                const Flexible(
+                  child: Text(
+                    overflow: TextOverflow.ellipsis,
+                    "Diğer kullanıcıların yorumları",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    overflow: TextOverflow.ellipsis,
+                    "Tümünü gör",
+                    style: TextStyle(
+                        color: AppTheme.primaryColor,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ]),
+              SizedBox(
+                height: 120,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 10,
+                    itemBuilder: (context, index) => const Padding(
+                        padding: EdgeInsets.only(right: 10),
+                        child: SizedBox(width: 300, child: Comment()))),
+              )
+            ]),
+          ),
         ],
       ),
     );
